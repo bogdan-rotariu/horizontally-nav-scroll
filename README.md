e3e9da62cf43a0896786b3fc981c437beda9bc54
Bogdan Rotariu <rotariubogdan@gmail.com>
2022-05-22 14:44:18 +0300

    Refactor/use refs (#1)
    * Chore: added demo link
    
    * [refactor] use Refs instead of DOM querying

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## This started as a proof-of-concept solution for a work related requirement of a horizontal self scrollable navigation bar, based on the current element visible in the viewport. Use it as you please if you find it useful.
 
+# Demo: https://horizontally-nav-scroll.vercel.app
+
 
 *This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).*
 
--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,23 +1,31 @@
+import { useCallback, useState } from 'react'
+import { useHorizontalScrollIntoView } from '../../hooks'
 import styles from './Nav.module.scss'
-import { useHorizontalScrollIntoView, useInViewElement } from '../../hooks'
-
-const ELEMENTS_SELECTOR = 'custom-attr'
 
 interface NavProps {
     items: number[]
+    inView: string | null
 }
 
-export const Nav = ({ items }: NavProps) => {
-    const inView = useInViewElement({ selector: ELEMENTS_SELECTOR })
+export const Nav = ({ items, inView }: NavProps) => {
+    const [inViewListEl, setInViewListEl] = useState<HTMLElement | null>(null)
+    useHorizontalScrollIntoView({ element: inViewListEl })
 
-    useHorizontalScrollIntoView({ selector: `[nav-attr="${inView}"]` })
+    const setRefCallback = useCallback(
+        (el: HTMLElement, i: string) => {
+            if (inView === i) {
+                setInViewListEl(el)
+            }
+        },
+        [inView]
+    )
 
     return (
         <ul className={styles.nav}>
             {items.map((i) => (
                 <li
                     key={i}
-                    nav-attr={`${i}`}
+                    ref={(el) => el && setRefCallback(el, String(i - 1))}
                     className={inView === String(i) ? styles.active : undefined}
                 >
                     Section {++i}
