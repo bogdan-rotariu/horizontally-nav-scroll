ce4179b85d1e103e7d46b48e2b98b59bf7519428
Bogdan Rotariu <bogdan.rotariu@ddroidd.com>
2022-11-07 10:52:33 +0200

    [fix]: the effect on useInViewElementObserver

--- hooks/use-in-view-element-observer/useInViewElementObserver.ts
+++ hooks/use-in-view-element-observer/useInViewElementObserver.ts
@@ -1,41 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 interface useInViewElementObserver {
-    elements: { [id: number]: HTMLElement } | null
-    topOffset?: number
+  elements: { [id: number]: HTMLElement } | null
+  topOffset?: number
 }
 
 export const useInViewElementObserver = ({
-    elements,
-    topOffset = 0,
+  elements,
+  topOffset = 0,
 }: useInViewElementObserver) => {
-    const [inView, setInView] = useState<string | null>(null)
+  const [inView, setInView] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (!elements) return
     const components = Object.entries(elements ?? [])
-   
+
     const callback = (entries: IntersectionObserverEntry[]) => {
-        const entry = entries?.[0]
-        components?.forEach(([id, element]) => {
-            if (entry?.isIntersecting && element === entry?.target) {
-                setInView(String(parseFloat(id) - 1))
-            }
-        })
+      const entry = entries?.[0]
+      components?.forEach(([id, element]) => {
+        if (entry?.isIntersecting && element === entry?.target) {
+          setInView(String(parseFloat(id) - 1))
+        }
+      })
     }
 
-    useEffect(() => {
-        if (!components) return
-        
-        const options = {
-            threshold: 0.75,
-            rootMargin: `${topOffset}px 0px 0px 0px`,
-        }
-        const observer = new IntersectionObserver(callback, options)
-        
-        components?.forEach(([, element]) => {
-            observer.observe(element)
-        })
+    const options = {
+      threshold: 0.75,
+      rootMargin: `${topOffset}px 0px 0px 0px`,
+    }
+    const observer = new IntersectionObserver(callback, options)
 
-        return () => observer.disconnect()
-    }, [components])
+    components?.forEach(([, element]) => {
+      observer.observe(element)
+    })
 
-    return inView
+    return () => observer.disconnect()
+  }, [elements])
+
+  return inView
 }
